cf48dab1583c663d170324dc964758d3ec9b17c1
memorez <133102039+memorez@users.noreply.github.com>
2023-05-19 00:46:19 +0900

    [Gold IV] Title: N-Queen, Time: 11744 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,20 +1,41 @@
 N=int(input())
 count=0
-def dfs(depth,lst,lst_u,lst_d):
+lst=[False for _ in range(N)]
+lst_u=[False for _ in range(2*N-1)]
+lst_d=[False for _ in range(2*N-1)]
+def dfs(depth):
     global count
     if depth==N:
         count+=1
         return
     if depth==0:
         for i in range(N//2):
-            dfs(depth+1,lst+[i],lst_u+[i+depth],lst_d+[i-depth])
+            lst[i]=True
+            lst_u[i+depth]=True
+            lst_d[depth-i+N-1]=True
+            dfs(depth+1)
+            lst[i]=False
+            lst_u[i+depth]=False
+            lst_d[depth-i+N-1]=False
         count*=2
         if N%2==1:
             i=N//2
-            dfs(depth+1,lst+[i],lst_u+[i+depth],lst_d+[i-depth])
+            lst[i]=True
+            lst_u[i+depth]=True
+            lst_d[depth-i+N-1]=True
+            dfs(depth+1)
+            lst[i]=False
+            lst_u[i+depth]=False
+            lst_d[depth-i+N-1]=False
     else:
         for i in range(N):
-            if not i in lst and not i+depth in lst_u and not i-depth in lst_d:
-                dfs(depth+1,lst+[i],lst_u+[i+depth],lst_d+[i-depth])
-dfs(0,[],[],[])
+            if not lst[i] and not lst_u[i+depth] and not lst_d[depth-i+N-1]:
+                lst[i]=True
+                lst_u[i+depth]=True
+                lst_d[depth-i+N-1]=True
+                dfs(depth+1)
+                lst[i]=False
+                lst_u[i+depth]=False
+                lst_d[depth-i+N-1]=False
+dfs(0)
 print(count)
