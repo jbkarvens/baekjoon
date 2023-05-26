c32037aa80d32d18b85d1e8fd31da378eae3024f
memorez <133102039+memorez@users.noreply.github.com>
2023-05-26 15:07:51 +0900

    [Gold II] Title: 피보나치 수 3, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.py
@@ -0,0 +1,15 @@
+def fib(n,m):
+    a,b=1,1
+    ls=[]
+    while n>1:
+        ls.append(n%2==1)
+        n>>=1
+    ls.reverse()
+    for tf in ls:
+        # F_2n+1 = F_n^2+F_n+1^2
+        # F_2n = F_n*(F_n+1 - F_n)+F_n+1 F_n
+        a,b=(a*(2*b-a))%m,(a*a+b*b)%m
+        if tf:
+            a,b=b,(a+b)%m
+    return a
+print(fib(int(input()),10**6))
