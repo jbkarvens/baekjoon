e6f5e0514a1ebcb95dae6d0ba259514664309fc8
memorez <133102039+memorez@users.noreply.github.com>
2023-05-30 02:19:11 +0900

    [Silver II] Title: 종이의 개수, Time: 4148 ms, Memory: 69440 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -1,31 +1,22 @@
-def dc(A):
-    n=len(A)
-    c=A[0][0]
+cout=[0,0,0]
+def dc(u,v,n):
+    c=A[u][v]
     chk=True
     for i in range(n*n):
-        if A[i//n][i%n]!=c:
+        if A[u+i//n][v+i%n]!=c:
             chk=False
             break
     if chk:
-        if c==-1:
-            return 1,0,0
-        elif c==0:
-            return 0,1,0
-        else:
-            return 0,0,1
-    a,b,c=0,0,0
-    for i in range(9):
-        a1,b1,c1=dc([r[(n//3)*(i%3):(n//3)*(i%3+1)] for r in A[(n//3)*(i//3):(n//3)*(i//3+1)]])
-        a+=a1
-        b+=b1
-        c+=c1
-    return a,b,c
+        cout[c+1]+=1
+    else:
+        for i in range(9):
+            dc(u+(i%3)*(n//3),v+(i//3)*(n//3),n//3)
 
 N=int(input())
 A=[]
 for _ in range(N):
     A.append(list(map(int,input().split())))
-a,b,c=dc(A)
-print(a)
-print(b)
-print(c)
+dc(0,0,len(A))
+print(cout[0])
+print(cout[1])
+print(cout[2])
