dfcd9fb1c96bf84b652358b47d405448425e22cf
memorez <133102039+memorez@users.noreply.github.com>
2023-05-30 16:42:40 +0900

    [Gold III] Title: 양팔저울, Time: 160 ms, Memory: 133028 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.py
+++ 백준/Gold/2629. 양팔저울/양팔저울.py
@@ -4,16 +4,16 @@ A=list(map(int,input().split()))
 M=int(input())
 X=list(map(int,input().split()))
 
-dp=[[False for _ in range(MAX_MASS)] for _ in range(N)]
-dp[0][0],dp[0][A[0]]=True,True
+dp=[False for _ in range(MAX_MASS)]
+dp[0],dp[A[0]]=True,True
 for i in range(1,N):
+    tmp=dp[:]
     for j in range(MAX_MASS):
-        if j+A[i]<MAX_MASS and dp[i-1][j+A[i]]:
-            dp[i][j]=True
-        elif abs(j-A[i])<MAX_MASS and dp[i-1][abs(j-A[i])]:
-            dp[i][j]=True
-        elif dp[i-1][j]:
-            dp[i][j]=True
+        if j+A[i]<MAX_MASS and dp[j+A[i]]:
+            tmp[j]=True
+        elif abs(j-A[i])<MAX_MASS and dp[abs(j-A[i])]:
+            tmp[j]=True
+    dp=[dp[i] or tmp[i] for i in range(MAX_MASS)]
             
-ans=['Y' if dp[-1][X[i]] else 'N' for i in range(M)]
+ans=['Y' if dp[X[i]] else 'N' for i in range(M)]
 print(*ans)
