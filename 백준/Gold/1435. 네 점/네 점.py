5abc3f3a9c3cbec2c132244c322bc1c12326db88
memorez <133102039+memorez@users.noreply.github.com>
2023-05-31 09:20:26 +0900

    [Gold II] Title: 네 점, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1435. 네 점/네 점.py
+++ 백준/Gold/1435. 네 점/네 점.py
@@ -1,23 +1,19 @@
-k=True
+k=1
 m=[list(map(int,input().split())) for _ in range(4)]
-x1=m[0][1]
-x2=m[0][2]
-x3=m[1][2]
-x4=m[0][3]
-x5=m[1][3]
-x6=m[2][3]
-a=x1
-b=(x2**2-x3**2+a**2)/(2*a)
-t=(2*a*x2)**2-(x2**2-x3**2+a**2)**2
-if t<0:
-    k=False
+x=m[0][1]
+y=m[0][2]
+z=m[1][2]
+u=m[0][3]
+v=m[1][3]
+w=m[2][3]
+b=y**2-z**2+x**2
+b/=2*x
+t=(2*x*y)**2-(y**2-z**2+x**2)**2
+if t<0:k=0
 else:
-    c=t**0.5/(2*a)
-    d=(a**2+x4**2-x5**2)/(2*a)
-    e=b**2+c**2-2*b*d-x6**2+x4**2
-    if 4*c**2*(x4**2-d**2)<e**2:
-        k=False
-if k:
-    print('YES')
-else:
-    print('NO')
+ c=t**0.5/(2*x)
+ d=(x**2+u**2-v**2)/(2*x)
+ e=b**2+c**2-2*b*d-w**2+u**2
+ if 4*c**2*(u**2-d**2)<e**2:
+  k=0
+print('YES'if k else'NO')
