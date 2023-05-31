76b0e69c783cf25e0faebbcd1495e7199c56cef8
jbkarvens <133102039+jbkarvens@users.noreply.github.com>
2023-06-01 02:05:50 +0900

    [Diamond I] Title: Hash, Time: 532 ms, Memory: 50664 KB -BaekjoonHub

--- 백준/Diamond/19376. Hash/Hash.py
+++ 백준/Diamond/19376. Hash/Hash.py
@@ -10,27 +10,22 @@ def HAIKU(s,A,B):
         h=(h*A+ord(s[i])-ord('a')+1)%B
     return h
 A,B=map(int,input().split())
-chk=set()
 lst=[]
-while len(lst)<7:
-    h_str_dict=dict()
-    while True:
-        s=rndstr(7)
-        if s in chk:
+h_str_dict=dict()
+while True:
+    s=rndstr(7)
+    h=HAIKU(s,A,B)
+    if h in h_str_dict:
+        s2=h_str_dict[h]
+        if s2==s:
             continue
-        h=HAIKU(s,A,B)
-        if h in h_str_dict:
-            s2=h_str_dict[h]
-            if s2==s:
-                continue
-            chk.add(s)
-            chk.add(s2)
-            lst.append([s,s2])
-            break
-        else:
-            h_str_dict[h]=s
+        lst.append(s)
+        lst.append(s2)
+        break
+    else:
+        h_str_dict[h]=s
 for i in range(100):
     s=''
     for j in range(7):
-        s+=lst[j][(i>>j)%2]
+        s+=lst[(i>>j)%2]
     print(s)
