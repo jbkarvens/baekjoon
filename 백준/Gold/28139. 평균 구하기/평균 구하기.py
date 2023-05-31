8e572130da7198772e4b25f3c7111bd07ea882d3
memorez <133102039+memorez@users.noreply.github.com>
2023-05-31 21:58:50 +0900

    [Gold IV] Title: 평균 구하기, Time: 1288 ms, Memory: 116092 KB -BaekjoonHub

--- 백준/Gold/28139. 평균 구하기/평균 구하기.py
+++ 백준/Gold/28139. 평균 구하기/평균 구하기.py
@@ -1,3 +1,7 @@
 N=int(input())
 c=[list(map(int,input().split())) for _ in range(N)]
-print(sum([((c[i][0]-c[j][0])**2+(c[i][1]-c[j][1])**2)**0.5 for i in range(N) for j in range(N)])/N)
+s=0
+for i in range(N):
+    for j in range(i+1,N):
+        s+=((c[i][0]-c[j][0])**2+(c[i][1]-c[j][1])**2)**0.5
+print(2*s/N)
