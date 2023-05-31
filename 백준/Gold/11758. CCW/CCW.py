e7c9c8c78c8ecfe720da5588f2e5c1102664883a
memorez <133102039+memorez@users.noreply.github.com>
2023-05-31 22:56:59 +0900

    [Gold V] Title: CCW, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.py
@@ -0,0 +1,7 @@
+x1,y1=map(int,input().split())
+x2,y2=map(int,input().split())
+x3,y3=map(int,input().split())
+z=(x1-x2)*(y3-y2)-(y1-y2)*(x3-x2)
+if z<0: print(1)
+elif z>0: print(-1)
+else: print(0)
