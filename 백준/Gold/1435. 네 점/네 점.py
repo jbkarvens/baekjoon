0f8f0c054b25a3c50a2cd9475520b1dde5743de7
memorez <133102039+memorez@users.noreply.github.com>
2023-05-31 09:07:39 +0900

    [Gold II] Title: 네 점, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1435. 네 점/네 점.py
+++ 백준/Gold/1435. 네 점/네 점.py
@@ -1,23 +1,23 @@
-chk=True
-d=[list(map(int,input().split())) for _ in range(4)]
-x=[None,0,0,0,0,0,0]
-x[1]=d[0][1]
-x[2]=d[0][2]
-x[3]=d[1][2]
-x[4]=d[0][3]
-x[5]=d[1][3]
-x[6]=d[2][3]
-a=x[1]
-b=(x[2]**2-x[3]**2+a**2)/(2*a)
-if 4*a**2*x[2]**2-(x[2]**2-x[3]**2+a**2)**2<0:
-    chk=False
+k=True
+m=[list(map(int,input().split())) for _ in range(4)]
+x1=m[0][1]
+x2=m[0][2]
+x3=m[1][2]
+x4=m[0][3]
+x5=m[1][3]
+x6=m[2][3]
+a=x1
+b=(x2**2-x3**2+a**2)/(2*a)
+t=(2*a*x2)**2-(x2**2-x3**2+a**2)**2
+if t<0:
+    k=False
 else:
-    c=(4*a**2*x[2]**2-(x[2]**2-x[3]**2+a**2)**2)**0.5/(2*a)
-    d=(a**2+x[4]**2-x[5]**2)/(2*a)
-    e=b**2+c**2-2*b*d-x[6]**2+x[4]**2
-    if 4*c**2*x[4]**2<4*c**2*d**2+e**2:
-        chk=False
-if chk:
+    c=t**0.5/(2*a)
+    d=(a**2+x4**2-x5**2)/(2*a)
+    e=b**2+c**2-2*b*d-x6**2+x4**2
+    if 4*c**2*(x4**2-d**2)<e**2:
+        k=False
+if k:
     print('YES')
 else:
     print('NO')
