f091eb6b6217114faeee42d6ace1977a27f3ec0f
jbkarvens <133102039+jbkarvens@users.noreply.github.com>
2023-06-04 03:13:05 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 540 ms, Memory: 63280 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(2*10**5+1)
+input=sys.stdin.readline
+
+def bfs(V,E,R):
+    for v in V:
+        if v==R:continue
+        visited[v]=False
+    visited[R]=True
+    ans[R]=cout[0]
+    cout[0]+=1
+    q=deque()
+    q.append(R)
+    while q:
+        u=q.popleft()
+        for v in E[u]:
+            if not visited[v]:
+                visited[v]=True
+                ans[v]=cout[0]
+                cout[0]+=1
+                q.append(v)
+    
+
+N,M,R=map(int,input().split())
+edge=[[] for _ in range(N+1)]
+visited=[None for _ in range(N+1)]
+ans=[0 for _ in range(N+1)]
+cout=[1]
+for _ in range(M):
+    u,v=map(int,input().split())
+    edge[u].append(v)
+    edge[v].append(u)
+for v in edge:
+    v.sort(reverse=True)
+bfs(list(range(1,N+1)),edge,R)
+for i in range(1,N+1):
+    print(ans[i])
