891086ac954270f6d4e41f551ec1e853a48bd3ef
jbkarvens <133102039+jbkarvens@users.noreply.github.com>
2023-06-04 18:31:22 +0900

    [Gold V] Title: 토마토, Time: 3520 ms, Memory: 58220 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -11,42 +11,38 @@ for z in range(H):
         lst[z].append(list(map(int,input().split())))
 
 q=deque()
-q_set=set()
 for k in range(H):
     for i in range(N):
         for j in range(M):
             if lst[k][i][j]==1:
-                q.append((k,i,j,0))
-                q_set.add((k,i,j))
-ans=[[[-1 for _ in range(M)] for _ in range(N)] for _ in range(H)]
+                q.append((k,i,j))
+ans=[[[None for _ in range(M)] for _ in range(N)] for _ in range(H)]
 for k in range(H):
     for i in range(N):
         for j in range(M):
-            if lst[k][i][j]==-1:
+            if lst[k][i][j]!=0:
                 ans[k][i][j]=0
             
 while q:
-    z,x,y,d=q.popleft()
-    q_set.remove((z,x,y))
-    ans[z][x][y]=d
+    z,x,y=q.popleft()
     for dz,dx,dy in MOVES:
         w,u,v=z+dz,x+dx,y+dy
-        if 0<=u<N and 0<=v<M and 0<=w<H and not ((w,u,v) in q_set) and lst[w][u][v]!=-1 and ans[w][u][v]==-1:
-            q.append((w,u,v,d+1))
-            q_set.add((w,u,v))
-
+        if 0<=u<N and 0<=v<M and 0<=w<H and ans[w][u][v]==None:
+            q.append((w,u,v))
+            ans[w][u][v]=ans[z][x][y]+1
+            
 days=0
 chk=False
 for k in range(H):
     for i in range(N):
         for j in range(M):
-            if ans[k][i][j]==-1:
+            if ans[k][i][j]==None:
                 chk=True
                 days=-1
                 break
-            days=max(days,ans[k][i][j])
         if chk:
             break
+        days=max(days,max(ans[k][i]))
     if chk:
         break
 print(days)
