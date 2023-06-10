dfaad86cc28c7b3176925b6c4a9e20692cee6799
jbkarvens <133102039+jbkarvens@users.noreply.github.com>
2023-06-10 22:30:47 +0900

    [Platinum III] Title: N!!!...! mod P, Time: 368 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Platinum/18373. N！！！．．．！ mod P/N！！！．．．！ mod P.py
@@ -0,0 +1,23 @@
+import sys
+input_func = sys.stdin.readline
+
+n,k,p=map(int,input_func().split())
+a=0
+if n==2:
+    a=2%p
+elif k==n==3:
+    k,n=2,6
+if k==2 and 2<n<=12:
+    for i in range(3,n+1):k*=i
+    if k<p:
+        if n==12:
+            t=1
+            for i in range(k+1,p):t=(t*i)%p
+            a=pow(-t,-1,p)
+        # elif n==11:
+        #     pass
+        else:
+            t=1
+            for i in range(2,k+1):t=(t*i)%p
+            a=t
+print(a)
