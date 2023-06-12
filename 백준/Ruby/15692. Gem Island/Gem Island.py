a59e4073760204f72ef68b4d32f475deb390053b
jbkarvens <133102039+jbkarvens@users.noreply.github.com>
2023-06-12 22:48:39 +0900

    [Ruby V] Title: Gem Island, Time: 1000 ms, Memory: 135484 KB -BaekjoonHub

--- 백준/Ruby/15692. Gem Island/Gem Island.py
+++ 백준/Ruby/15692. Gem Island/Gem Island.py
@@ -1,15 +1,10 @@
 import sys
-import math
 input_func = sys.stdin.readline
 
 n,d,r=map(int,input_func().split())
 
-# exp[people][day] : day일 후 상위 r명의 gem수의 expectation(사람 수=people)
-exp = [[None for _ in range(d+1)] for _ in range(n+1)]
-for i in range(d+1):
-    exp[r][i] = float(i)
-for i in range(n+1):
-    exp[i][0] = float(0)
+# gem[people][day] : 가능한 조합에서 day일 후 상위 r명의 gem수들 합(사람 수=people)
+gem = [[None for _ in range(d+1)] for _ in range(n+1)]
 
 N_MAX = 1000
 binom = [[0 for _ in range(N_MAX+1)] for _ in range(N_MAX+1)]
@@ -19,20 +14,18 @@ for i in range(N_MAX+1):
     for j in range(1,i):
         binom[i][j] = binom[i-1][j] + binom[i-1][j-1]
 
-for peop in range(r+1,n+1):
-    for day in range(1,d+1):
-        res = 0
-        # poor : peop명 중 최소 gem 값
-        # cout : poor만큼의 gem 가진 사람 수
-        for i in range(day+1):
-            poor = i//peop
-            cout = peop - i%peop
-            prob = binom[peop][cout]*binom[day-peop*poor-1][peop-cout-1]
-            if cout>=peop-r:
-                gems = day - (peop - r)*poor
-            else:
-                gems = exp[peop-cout][day-(1+poor)*peop+cout] + r*(poor+1)
-            res+=gems*prob
-        exp[peop][day] = res/binom[peop+day-1][day]
+for i in range(r,n+1):
+    gem[i][0] = float(r)
+for i in range(d+1):
+    for j in range(r+1):
+        gem[j][i] = float(i+j)*binom[i+j-1][i]
 
-print(exp[n][d]+r)
+for peop in range(r+1,n+1):
+    for day in range(0,d+1):
+        res = 0
+        # i : gem들 나눠줬을 때 기본 gem 하나만 가진 사람 수
+        for i in range(max(0,peop-day),peop+1):
+            res += binom[peop][i] * gem[peop - i][day - peop + i]
+        gem[peop][day] = res + r*binom[peop+day-1][day]
+
+print(gem[n][d]/binom[n+d-1][d])
