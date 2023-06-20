161bf8886f613ea710ab33d21eed23a9606062ca
jbkarvens <133102039+jbkarvens@users.noreply.github.com>
2023-06-20 20:22:06 +0900

    [Silver III] Title: 문자열 집합, Time: 5612 ms, Memory: 2096968 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,26 +1,34 @@
-def BinSearch(arr,tar):
-    a,b=0,len(arr)-1
-    m=(a+b)//2
-    while a<=b:
-        if arr[m]<tar:
-            a=m+1
-            m=(a+b)//2
-        elif arr[m]>tar:
-            b=m-1
-            m=(a+b)//2
-        else:
-            return m
-    return None
-N,M=map(int,input().split())
-lst_S=[]
-for _ in range(N):
-    lst_S.append(input())
-lst_test=[]
-for _ in range(M):
-    lst_test.append(input())
-lst_S.sort()
-cout=0
-for tar in lst_test:
-    if BinSearch(lst_S,tar) is not None:
-        cout+=1
-print(cout)
+import sys
+input = sys.stdin.readline
+
+class Node:
+    def __init__(self,end=False):
+        self.end = end
+        self.child = {}
+
+def add(start,word):
+    cur_node = start
+    for c in word:
+        if c not in cur_node.child:
+            cur_node.child[c] = Node()
+        cur_node = cur_node.child[c]
+    cur_node.end = True
+
+def find(start,word):
+    cur_node = start
+    for c in word:
+        if c not in cur_node.child:
+            return False
+        cur_node = cur_node.child[c]
+    return cur_node.end
+
+if __name__=='__main__':
+    start = Node()
+    N,M=map(int,input().split())
+    for _ in range(N):
+        add(start,input().rstrip())
+    ans = 0
+    for _ in range(M):
+        if find(start,input().rstrip()):
+            ans+=1
+    print(ans)
