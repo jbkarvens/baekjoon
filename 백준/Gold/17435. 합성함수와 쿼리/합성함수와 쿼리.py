b88bbe9280bcd51e3e6be9e8cfb7c63f81858d68
jbkarvens <133102039+jbkarvens@users.noreply.github.com>
2023-06-23 00:47:27 +0900

    [Gold I] Title: 합성함수와 쿼리, Time: 1788 ms, Memory: 253172 KB -BaekjoonHub

--- 백준/Gold/17435. 합성함수와 쿼리/합성함수와 쿼리.py
+++ 백준/Gold/17435. 합성함수와 쿼리/합성함수와 쿼리.py
@@ -5,7 +5,7 @@ if __name__=='__main__':
     M = int(input())
     F = [None]+[*map(int, input().split())]
     sparse_table = [[F[i]] for i in range(M + 1)]
-    depth = 20
+    depth = 19
     for i in range(depth):
         for j in range(1,M+1):
             sparse_table[j].append(sparse_table[sparse_table[j][i]][i])
@@ -16,4 +16,4 @@ if __name__=='__main__':
             if n&(1<<i)!=0:
                 x = sparse_table[x][i]
             n&=~(1<<i)
-        print(x)
+        sys.stdout.write(f'{x}\n')
