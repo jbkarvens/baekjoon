56a46d18c2793463c12a28d385ee50afde40ebb2
jbkarvens <133102039+jbkarvens@users.noreply.github.com>
2023-06-23 23:24:50 +0900

    [Diamond IV] Title: DivModulo, Time: 7016 ms, Memory: 117184 KB -BaekjoonHub

---
+++ 백준/Diamond/18151. DivModulo/DivModulo.py
@@ -0,0 +1,159 @@
+import sys
+input=sys.stdin.readline
+import math
+from random import randint
+
+MR_TEST_LIST = [2,3,5,7,11,13,17,19]
+SMALL_PRIME = 100
+
+def is_small_prime(n):
+    if n<=1:
+        return False
+    elif n==2:
+        return True
+    elif n%2==0:
+        return False
+    i=3
+    while i*i<=n:
+        if n%i==0: return False
+        i+=2
+    return True
+
+def isPrime(n):
+    s,tmp=0,n-1
+    while tmp&1==0:
+        tmp>>=1
+        s+=1
+    d=(n-1)>>s
+    for a in MR_TEST_LIST:
+        a_witness=False
+        cal=pow(a,d,n)
+        if cal==1 or cal==n-1:
+            a_witness=True
+            continue
+        for _ in range(1,s):
+            cal=pow(cal,2,n)
+            if cal==n-1:
+                a_witness=True
+        if not a_witness:
+            return False
+    return True
+    
+def brent(n):
+    y,c=randint(1,n-1),randint(1,n-1)
+    g,r,q=1,1,1
+    while g==1:
+        x=y
+        for _ in range(r):
+            y=(y*y+c)%n
+        ys=y
+        for i in range(r):
+            y=(y*y+c)%n
+            q=(q*(x-y))%n
+        g=math.gcd(q,n)
+        r*=2
+    if g==n:
+        while True:
+            ys=(ys*ys+c)%n
+            g=math.gcd(x-ys,n)
+            if g>1:
+                break
+    return g
+
+def factor(n):
+    if n<=1:
+        return []
+    if n%2==0:
+        return [2]+factor(n//2)
+    for i in range(3,SMALL_PRIME,2):
+        if n%i==0:
+            return [i]+factor(n//i)
+    if isPrime(n):
+        return [n]
+    d = brent(n)
+    return factor(d) + factor(n//d)
+
+def crt(rem,mod):
+    tot = 1
+    for m in mod:
+        tot *= m
+    res = 0
+    for r,m in zip(rem,mod):
+        mm=tot//m
+        res+=r*mm*pow(mm,-1,m)
+    return res%tot
+
+def naive_factorial(n,mod,p):
+    res = 1
+    for i in range(2,n+1):
+        if i%p==0:
+            continue
+        res = (res*i)%mod
+    return res
+
+# p 배수 제외하고 곱한 팩토리얼
+def factorial_without_p(n,p,e):
+    if p==2:
+        if e==1:
+            return 1
+        elif e==2:
+            mod =4
+            q,r=divmod(n,mod)
+            res = pow(-1,q)
+            if r==3:
+                res = -res
+            return res%mod
+    pp=pow(p,(e+1)//2)
+    q,r=divmod(n,pp)
+    mod=pow(p,e)
+    res = pow(naive_factorial(pp-1,mod,p),q,mod)
+    for i in range(q*pp+1,n+1):
+        if i%p==0:
+            continue
+        res = (res*i)%mod
+    return res
+
+def factorial_dmod(n,p,e):
+    res = 1
+    while n>0:
+        res*=factorial_without_p(n, p,e)
+        n//=p
+    return res
+
+def factorial_pval(n,p):
+    res = 0
+    while n>0:
+        n//=p
+        res+=n
+    return res
+
+def binom(m,n,d):
+    primes=factor(d)
+    factors=dict()
+    for p in primes:
+        if p in factors:
+            factors[p]+=1
+        else:
+            factors[p]=1
+    mod = []
+    rem = []
+    pval=[]
+    pval_d = []
+    prime_list = []
+    for p in factors:
+        e = factors[p]
+        pp=pow(p,e)
+        mod.append(pp)
+        rem.append((factorial_dmod(m,p,e)*pow(factorial_dmod(n,p,e)*factorial_dmod(m-n,p,e),-1,pp))%pp)
+        pval.append(factorial_pval(m,p)-factorial_pval(n,p)-factorial_pval(m-n, p))
+        pval_d.append(factors[p])
+        prime_list.append(p)
+    h = min([e//f for e,f in zip(pval,pval_d)])
+    for i in range(len(pval)):
+        pval[i]-=h*pval_d[i]
+        rem[i]=(rem[i]*pow(prime_list[i],pval[i])*pow(d//mod[i],-h,mod[i]))%mod[i]
+    return crt(rem,mod)
+
+if __name__=='__main__':
+    m,n,d=map(int, input().split())
+    print(binom(m,n,d))
---
+++ 백준/Diamond/18151. DivModulo/README.md
@@ -0,0 +1,26 @@
+# [Diamond IV] DivModulo - 18151 
+
+[문제 링크](https://www.acmicpc.net/problem/18151) 
+
+### 성능 요약
+
+메모리: 117184 KB, 시간: 7016 ms
+
+### 분류
+
+중국인의 나머지 정리, 분할 정복을 이용한 거듭제곱, 수학, 모듈로 곱셈 역원, 정수론
+
+### 문제 설명
+
+<p>Modulo (mod) is a very common operator on integers. For two integers n and d with d > 0, r ≡ (n mod d) is defined where 0 ≤ r < d and there exists an integer q, such that n = q × d+r. For example, (200 mod 5) ≡ 0 means that the remainder of 200 divided by 5 is 0. Here is another new operator called DivModulo (dmod) defined as follows. For two integers n and d with d > 0, r ≡ (n dmod d) is defined where there exists two integers m and h, such that r ≡ (m mod d), n = m × d<sup>h</sup>, and d is not a factor of m. For example, (200 dmod 5) ≡ 3, since (200 dmod 5) ≡ (8 × 5<sup>2</sup> dmod 5) ≡ (8 mod 5) ≡ 3.</p>
+
+<p>Consider the factorials and the combination function. For an integer M ≥ 0, the factorial M! is defined as M! = M × (M −1) × (M −2) × · · · × 3 × 2 × 1, and 0! = 1 is defined. For integers M and N with 0 ≤ N ≤ M, the combination function C(M, N) is defined as C(M, N) = M!/(N!×(M−N)!). Now given three integers M, N, D with D > 0, please compute C(M, N) dmod D. For example, (C(9, 2) dmod 3) ≡ (36 dmod 3) ≡ (4 × 3<sup>2</sup> dmod 3) ≡ (4 mod 3) ≡ 1.</p>
+
+### 입력 
+
+ <p>Three integers M, N and D are given in one line.</p>
+
+### 출력 
+
+ <p>Please output C(M, N) dmod D in one line.</p>
+
