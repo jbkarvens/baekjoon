ed2486d81a68b129c1f8041b7500828af73cd7c0
jbkarvens <133102039+jbkarvens@users.noreply.github.com>
2024-12-24 15:46:52 +0900

    [Platinum V] Title: 노래방, Time: 1360 ms, Memory: 111516 KB -BaekjoonHub

--- 백준/Platinum/12932. 노래방/노래방.py
+++ 백준/Platinum/12932. 노래방/노래방.py
@@ -1,11 +1,8 @@
 N=int(input())
-arr=list(map(int,input().split()))
-arr=[0]+arr
-score=[[0 for _ in range(i)] for i in range(N+1)]
+a=[0]+list(map(int,input().split()))
+s=[[0]*i for i in range(N+1)]
 for i in range(2,N+1):
-    for j in range(i):
-        if j==i-1:
-            score[i][j]=min([score[i-1][k]+abs(arr[i]-arr[k]) for k in range(1,i-1)]+[score[i-1][0]])
-        else:
-            score[i][j]=score[i-1][j]+abs(arr[i]-arr[i-1])
-print(min(score[N]))
+    for j in range(i-1):
+        s[i][j]=s[i-1][j]+abs(a[i]-a[i-1])
+    s[i][i-1]=min([s[i-1][k]+abs(a[i]-a[k]) for k in range(1,i-1)]+[s[i-1][0]])
+print(min(s[N]))
