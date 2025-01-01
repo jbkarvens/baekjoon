ee04929488fb0a684db381a4e8dee4cea3a167b9
jbkarvens <133102039+jbkarvens@users.noreply.github.com>
2025-01-01 12:49:18 +0900

    [Gold V] Title: RUN Number, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31813. RUN Number/RUN Number.py
+++ 백준/Gold/31813. RUN Number/RUN Number.py
@@ -1,19 +1,27 @@
 import sys
 input=sys.stdin.readline
 
-def solve(n):
+run=[]
+for n in range(1,19):
+    for e in range(1,10):
+        run.append(e*(pow(10,n)-1)//9)
+
+def solve(n,k):
     if n==0:
         return []
-    e=1
-    while e<=n:
-        e=10*e+1
-    e//=10
-    e*=(n//e)
-    return [e]+solve(n-e)
-    
+    if k==0:
+        return False
+    for i in range(len(run)):
+        if n<run[i]:
+            break
+    while i>=0:
+        i-=1
+        arr=solve(n-run[i],k-1)
+        if arr!=False:
+            return [run[i]]+arr
 
 for _ in range(int(input())):
     N,K=map(int,input().split())
-    arr=solve(K)
+    arr=solve(K,1+N)
     print(len(arr))
     print(*arr)
